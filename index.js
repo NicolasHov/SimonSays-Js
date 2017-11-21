9ffc77a0c7b361d9b609a751d8cea53b435a57ca
NicolasHov <kola.hov@gmail.com>
2017-11-21 18:10:01 +0100

    FC

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// var link = document.getElementById('myLink');
+//
+// var href = link.getAttribute('href'); // On récupère l'attribut « href »
+//
+// // alert(href);
+// link.setAttribute('href', 'http://www.siteduzero.com'); // On édite l'attribut « href »
+//
+// alert("qfqsdqs");
+//
+
+
+function playSequence() {
+  var elem = document.getElementById("1");
+  elem.style.background-color = "#fc5d3d";
+console.log("test");
+  // var id = setInterval(frame, 5);
+  // function frame() {
+  //     clearInterval(id);
+  //   }
+}
